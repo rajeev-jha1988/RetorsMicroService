793aa4e1b95736c4345ba35c28973ba1b5e2f040
Rajeev\raj007 <raj.k9732@gmail.com>
2017-07-27 01:07:54 +0530

    Oracle Table
    Change-Id: I7ee7adcc5f3c99831c08f50ce98c03c965b76fb6
    Signed-off-by: Rajeev\raj007 <raj.k9732@gmail.com>

---
+++ src/main/resources/oracletable.sql
@@ -0,0 +1,10 @@
+505 678 072
+
+2681
+
+
+2017.06.16 08:20:00
+
+
+
+ALL_PROJECTED_ATTRIBUTES can be used only when Querying using an IndexName (Service: AmazonDynamoDBv2; Status Code: 400; Error Code: ValidationException; Request ID: RNI6RKH6CT0KKKR9OP801LMII7VV4KQNSO5AEMVJF66Q9ASUAAJG)
